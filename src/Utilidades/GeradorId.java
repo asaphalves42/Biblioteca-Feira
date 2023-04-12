3008f2eb767d838a5ebe0329d046ac1b57db010c
David Fernandes <1221777@isep.ipp.pt>
2023-04-12 20:17:55 +0100

    Utilidade para gerar ID's

--- src/Utilidades/GeradorId.java
+++ src/Utilidades/GeradorId.java
@@ -1,2 +1,11 @@
-package Utilidades;public class GeradorId {
+package Utilidades;
+
+import java.util.UUID;
+
+public class GeradorId {
+
+    public static String Novo(){
+        UUID guid = UUID.randomUUID();
+        return guid.toString();
+    }
 }
