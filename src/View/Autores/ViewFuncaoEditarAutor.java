406c3bb53059cccd8d2987a9a1012014f993a729
David Fernandes <1221777@isep.ipp.pt>
2023-04-12 20:17:14 +0100

    Correções na função editar autor

--- src/View/Autores/ViewFuncaoEditarAutor.java
+++ src/View/Autores/ViewFuncaoEditarAutor.java
@@ -2,26 +2,37 @@ package View.Autores;
 
 import Controller.ControllerAutores;
 import Model.Autor;
+import Utilidades.Leitura;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
 
-import static Utilidades.Leitura.*;
+import static Utilidades.Leitura.LeInt;
+import static Utilidades.Leitura.LeStr;
+
 public class ViewFuncaoEditarAutor {
 
     public void editarAutor(ControllerAutores gestor) {
 
-        String idAutor = LeStr("Insira o Id do(a) autor(a) que quer editar");
-        ArrayList<Autor> autorEditar = gestor.pesquisarAutorPorId(idAutor);
+        int idAutor = LeInt("Insira o Id do(a) autor(a) que quer editar");
+        Autor autorEditar = gestor.pesquisarAutorPorId(idAutor);
 
-        if (autorEditar.isEmpty()) {
+        if (autorEditar == null) {
             System.out.println("Não existem autores com esse Id");
             System.out.println(" ");
-        } else {
-            for (Autor autor : autorEditar) {
-                System.out.println(autor);
+        }else {
+            String novoNome = LeStr("insira o novo nome do(a) autor(a)");
+            String novaMorada = LeStr("insira a nova morada");
+            LocalDate novaDataDeNascimento = Leitura.LeData();
+
+            boolean editado = gestor.editarAutor(idAutor, novoNome, novaMorada, novaDataDeNascimento);
+
+            if (editado) {
+                System.out.println("Autor editado com sucesso");
+                System.out.println(" ");
+            } else {
+                System.out.println("Autor não editado");
+                System.out.println(" ");
             }
         }
-
-        //int idAutor = LeStr("Insira o Id d")
-    }
+   }
 }
