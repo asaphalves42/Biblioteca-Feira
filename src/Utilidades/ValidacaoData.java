e5ac7f97a14f6fdc36293ee1a1bce6cbd5e5fe8d
Asaph Alves <1221473@isep.ipp.pt>
2023-04-13 23:58:48 +0100

    gestor de ficheiros, correção

--- src/Utilidades/ValidacaoData.java
+++ src/Utilidades/ValidacaoData.java
@@ -1,32 +1,13 @@
 package Utilidades;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
+
 
 import static Utilidades.Leitura.ler;
 
 public class ValidacaoData {
-
-
-
-    //Metodo retirado da internet para ler datas https://stackoverflow.com/questions/27580655/how-to-set-a-date-as-input-in-java
-
-    public Date LerData(String date) {
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        Date date2 = null;
-        try {
-            //Parsing the String
-            date2 = dateFormat.parse(date);
-        } catch (ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return (date2);
-    }
     public LocalDate LerData2() {
         try {
             LocalDate data;
