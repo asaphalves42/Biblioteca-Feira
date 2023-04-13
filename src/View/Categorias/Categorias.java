9d22b5ba95926a88f9379af908e065680e26308f
Asaph Alves <1221473@isep.ipp.pt>
2023-04-13 23:59:23 +0100

    Função para adicionar as categorias

--- src/View/Categorias/Categorias.java
+++ src/View/Categorias/Categorias.java
@@ -1,2 +1,39 @@
-package View.Categorias;public class Categorias {
+package View.Categorias;
+
+import static Utilidades.Leitura.ler;
+
+public class Categorias {
+    public int escolherCategorias() {
+        int opcao;
+        do {
+            System.out.println("Categoria: ");
+            System.out.println("1 - Artes");
+            System.out.println("2 - Ciencia");
+            System.out.println("3 - Matematica");
+            System.out.println("4 - Psicologia");
+            System.out.println("5 - Tecnologia");
+
+            opcao = ler.nextInt();
+            ler.nextLine();
+
+            switch (opcao){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+        }
+        while(opcao !=5);
+        return opcao;
+
+
+    }
 }
+
+
