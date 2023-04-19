2a09c5d9c6424f3a8ee842b235b6a31289dd7083
Asaph Alves <1221473@isep.ipp.pt>
2023-04-19 11:31:51 +0100

    correções estéticas

--- src/View/Socios/ViewFuncaoPesquisarSocio.java
+++ src/View/Socios/ViewFuncaoPesquisarSocio.java
@@ -17,7 +17,7 @@ public class ViewFuncaoPesquisarSocio {
         ArrayList<Socio> socio = gestor.pesquisarSocioPorNome(nome);
 
         if (socio.isEmpty()) {
-            System.out.println("Socio inexistente!");
+            System.out.println("Socio(a) inexistente!");
         } else {
             System.out.println(gestor.pesquisarSocioPorNome(nome));
         }
@@ -25,12 +25,12 @@ public class ViewFuncaoPesquisarSocio {
 
     public void pesquisarSocioPorNumMecanografico(ControllerSocios gestor) {
 
-        String numMecanografico = leStr("Insira o Id do(a) autor(a)");
+        String numMecanografico = leStr("Insira o Id do(a) sócio(a)");
 
         ArrayList<Socio> socio = gestor.pesquisarSocioPorNumMecanografico(numMecanografico);
 
         if (socio.isEmpty()) {
-            System.out.println("Autor(a) inexistente!");
+            System.out.println("Socio(a) inexistente!");
         } else {
             System.out.println(gestor.pesquisarSocioPorNumMecanografico(numMecanografico));
         }
