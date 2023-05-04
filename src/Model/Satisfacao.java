bc56c1f6f20837d662c29227b1307e16621d01f0
Asaph Alves <1221473@isep.ipp.pt>
2023-05-04 19:56:53 +0100

    correção txt

--- src/Model/Satisfacao.java
+++ src/Model/Satisfacao.java
@@ -2,14 +2,22 @@ package Model;
 
 public class Satisfacao {
     private int satisfacao;
+    private String observacao;
 
     public void setSatisfacao(int satisfacao) {
         this.satisfacao = satisfacao;
     }
 
+
     public int getSatisfacao() {
         return satisfacao;
     }
 
-}
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    public String getObservacao() {
+        return observacao;
+    }
 
+}
